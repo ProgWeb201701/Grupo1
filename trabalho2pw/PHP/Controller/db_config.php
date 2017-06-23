6c01a3d8f64f1711e7edbde57942dec5b6b85600
Unknown <danimarttiins@gmail.com>
2017-06-22 22:52:56 -0300

    Alteração no nome do banco e renomei o nome da classe de configuração do banco

--- trabalho2pw/PHP/Controller/db_config.php
+++ trabalho2pw/PHP/Controller/db_config.php
@@ -1,18 +1,16 @@
+
+
 <?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'duo');
-class DB_con {
-    public $connection;
-    function __construct(){
-        $this->connection = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD,DB_DATABASE);
+$link = mysqli_connect("localhost", "root", "", "pw");
 
-        if ($this->connection->connect_error) die('Database error -> ' . $this->connection->connect_error);
-
-    }
-
-    function ret_obj(){
-        return $this->connection;
-    }
+if (!$link) {
+    echo "Error: Falha ao conectar-se com o banco de dados MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
 }
+
+echo "Sucesso: Sucesso ao conectar-se com a base de dados MySQL." . PHP_EOL;
+
+mysqli_close($link);
+?>
